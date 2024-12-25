3513e561a23b20707dfb709ad85c251f7e7d3fce
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2024-12-25 13:02:48 +0530

    React app and SpringBoot app created
    Frontend was set using ReactJS
    Backend was set using SpringBoot

---
+++ backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/BackendPharmaPulseApplication.java
@@ -0,0 +1,13 @@
+package com.group03.backend_PharmaPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPharmaPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPharmaPulseApplication.class, args);
+	}
+
+}
