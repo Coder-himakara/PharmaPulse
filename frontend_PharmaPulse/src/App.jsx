a2f969e5e5d64f093d8ba1fc588d593cfa058d83
Azha <157900694+AzhaFauz@users.noreply.github.com>
2025-01-10 13:57:40 +0530

    Create Responsiveness Component and update dashboard.jsx

--- frontend_PharmaPulse/src/App.jsx
+++ frontend_PharmaPulse/src/App.jsx
@@ -1,8 +1,10 @@
 import Footer from "./components/Footer/Footer";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Navbar from "./components/Navbar/Navbar";
-import "./App.css";
 import DashboardCard from "./components/DashboardCard/DashboardCard";
+
+import "./App.css";
+//import "./Responsiveness/Responsive.css";
 
 function App() {
   return (
