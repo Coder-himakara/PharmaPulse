a09707e13ce5752773ab76652d00007d7ccc9107
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2025-01-13 13:19:13 +0530

    Purchase module: added getAll purchaseGroups-1
    Created controller method , added dto and entity classes, created service class

---
+++ backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/purchase/service/PurchaseGroupService.java
@@ -0,0 +1,9 @@
+package com.group03.backend_PharmaPulse.purchase.service;
+
+import com.group03.backend_PharmaPulse.purchase.entity.PurchaseGroup;
+
+import java.util.Collection;
+
+public interface PurchaseGroupService {
+    public abstract Collection<PurchaseGroup> getAllPurchaseGroups();
+}
