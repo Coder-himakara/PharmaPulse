269e432f575dc0d810ee800f2df08e5b2ec06ed1
Azha <157900694+AzhaFauz@users.noreply.github.com>
2025-01-17 20:47:28 +0530

    Added tailwind css for Footer.jsx and deleted Footer.css

--- frontend_PharmaPulse/src/components/Footer/Footer.jsx
+++ frontend_PharmaPulse/src/components/Footer/Footer.jsx
@@ -1,17 +1,18 @@
-import './Footer.css';
 import logo from '../../assets/Logo.jpg';
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div className='footer-content'>
-        <div className='footer-logo'>
-          <img src={logo} alt='Logo' className='logo' />
+    <footer className='bg-[#c1ddc1] text-[#333] p-4 fixed bottom-0 left-0 w-full box-border flex justify-center items-center z-50 h-[70px]'>
+      <div className='flex justify-between items-center w-full max-w-[1800px]'>
+        <div className='flex items-center'>
+          <img src={logo} alt='Logo' className='h-[30px] mr-2' />
         </div>
-        <div className='footer-left'>
+        <div className='text-sm text-left'>
           &copy; A &amp; K Agencies PharmaPulse. All Rights Reserved.
         </div>
-        <div className='footer-right'>Developed by: Team PharmaPulse</div>
+        <div className='flex gap-5 items-center text-sm font-bold'>
+          Developed by: Team PharmaPulse
+        </div>
       </div>
     </footer>
   );
