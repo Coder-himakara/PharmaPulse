65c16f30b82be6b3a3002265df30422e20fedec6
Azha <157900694+AzhaFauz@users.noreply.github.com>
2025-01-17 20:44:53 +0530

    Added tailwind css for DashboardCard.jsx and deleted DashboardCard.css

--- frontend_PharmaPulse/src/components/DashboardCard/DashboardCard.jsx
+++ frontend_PharmaPulse/src/components/DashboardCard/DashboardCard.jsx
@@ -1,10 +1,11 @@
-import './DashboardCard.css';
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const DashboardCard = ({ content }) => {
   return (
-    <div className='dashboard-card'>
-      <h1>Welcome to PharmaPulse!</h1>
+    <div className="bg-[#e6f6f6] absolute top-[100px] bottom-[70px] left-[250px] right-0 p-5 box-border overflow-y-auto">
+      <h1 className="text-5xl font-bold text-[#2a4d69] mb-4">
+        Welcome to PharmaPulse!
+      </h1>
       <div>{content}</div>
     </div>
   );
