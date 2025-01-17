f973eff090a1705525f8cffbd450b89c5d6c3d5c
Azha <157900694+AzhaFauz@users.noreply.github.com>
2025-01-17 20:49:37 +0530

    updated

--- frontend_PharmaPulse/src/components/Navbar/Navbar.jsx
+++ frontend_PharmaPulse/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { FaUser, FaSignOutAlt, FaTachometerAlt } from "react-icons/fa";
-import logo from "../../assets/Logo.jpg";
+import { useState } from 'react';
+import { FaUser, FaSignOutAlt, FaTachometerAlt } from 'react-icons/fa';
+import logo from '../../assets/Logo.jpg';
 
 const Navbar = () => {
   // State to manage dropdown visibility
@@ -11,47 +11,47 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between items-center bg-[#1a5353] p-4 text-white fixed top-0 left-0 w-full z-10 h-[100px]">
-      <div className="flex items-center">
-        <img src={logo} alt="Logo" className="h-[40px] mr-9 ml-9" />
-        <span className="text-lg font-bold">Home</span>
+    <div className='flex justify-between items-center bg-[#1a5353] p-4 text-white fixed top-0 left-0 w-full z-10 h-[100px]'>
+      <div className='flex items-center'>
+        <img src={logo} alt='Logo' className='h-[40px] mr-9 ml-9' />
+        <span className='text-lg font-bold'>Home</span>
       </div>
 
-      <div className="flex items-center gap-2 relative mr-9">
+      <div className='flex items-center gap-2 relative mr-9'>
         <div>
-          <span className="text-sm font-bold">John Doe</span>
+          <span className='text-sm font-bold'>John Doe</span>
           <br />
-          <span className="text-xs italic">Admin</span>
+          <span className='text-xs italic'>Admin</span>
         </div>
-        <div className="relative ml-4">
+        <div className='relative ml-4'>
           <button
             onClick={toggleDropdown}
-            className="bg-transparent border-none text-white text-base cursor-pointer flex items-center hover:text-[#c3e4e5]"
+            className='bg-transparent border-none text-white text-base cursor-pointer flex items-center hover:text-[#c3e4e5]'
           >
             ▼
           </button>
           {/* Show dropdown if isDropdownVisible is true */}
           {isDropdownVisible && (
-            <div className="absolute top-full right-0 bg-white text-[#333] border border-[#ddd] rounded-md min-w-[150px] shadow-lg z-10">
+            <div className='absolute top-full right-0 bg-white text-[#333] border border-[#ddd] rounded-md min-w-[150px] shadow-lg z-10'>
               <a
-                href="#dashboard"
-                className="flex items-center p-2 text-sm text-[#333] hover:bg-[#f1f1f1]"
+                href='#dashboard'
+                className='flex items-center p-2 text-sm text-[#333] hover:bg-[#f1f1f1]'
               >
-                <FaTachometerAlt className="w-5 h-5 mr-2" />
+                <FaTachometerAlt className='w-5 h-5 mr-2' />
                 Dashboard
               </a>
               <a
-                href="#profile"
-                className="flex items-center p-2 text-sm text-[#333] hover:bg-[#f1f1f1]"
+                href='#profile'
+                className='flex items-center p-2 text-sm text-[#333] hover:bg-[#f1f1f1]'
               >
-                <FaUser className="w-5 h-5 mr-2" />
+                <FaUser className='w-5 h-5 mr-2' />
                 Profile
               </a>
               <a
-                href="#logout"
-                className="flex items-center p-2 text-sm text-[#333] hover:bg-[#f1f1f1]"
+                href='#logout'
+                className='flex items-center p-2 text-sm text-[#333] hover:bg-[#f1f1f1]'
               >
-                <FaSignOutAlt className="w-5 h-5 mr-2" />
+                <FaSignOutAlt className='w-5 h-5 mr-2' />
                 Log Out
               </a>
             </div>
--- frontend_PharmaPulse/src/pages/EmployeeDashboard/EmployeeDashboard.jsx
+++ frontend_PharmaPulse/src/pages/EmployeeDashboard/EmployeeDashboard.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "../../components/Navbar/Navbar";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import DashboardCard from "../../components/DashboardCard/DashboardCard";
-import Footer from "../../components/Footer/Footer";
-import AppRoutes from "../../routes/AppRoutes";
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Navbar';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import DashboardCard from '../../components/DashboardCard/DashboardCard';
+import Footer from '../../components/Footer/Footer';
+import AppRoutes from '../../routes/AppRoutes';
 
 const EmployeeDashboard = () => {
   return (
-    <div className="employee-dashboard">
+    <div className='employee-dashboard'>
       <Router>
         <Navbar />
         <Sidebar />
-        <div className="dashboard-content">
+        <div className='dashboard-content'>
           <DashboardCard content={<AppRoutes />} />
         </div>
         <Footer />
