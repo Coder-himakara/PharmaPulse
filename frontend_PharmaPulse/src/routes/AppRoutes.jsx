f510ceaa0150488f7074e7eba55fcabdc37873d2
Azha <157900694+AzhaFauz@users.noreply.github.com>
2025-01-17 19:05:20 +0530

    Create AppRoutes.jsx and include the routes related to the EmployeeDashboard

---
+++ frontend_PharmaPulse/src/routes/AppRoutes.jsx
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import AddProductsForm from '../components/Forms/AddProductsForm';
+import ProductsInfoTable from '../components/Tables/ProductsInfoTable';
+import AddCustomersForm from '../components/Forms/AddCustomersForm';
+import SuppliersInfoTable from '../components/Tables/SuppliersInfoTable';
+import AddSuppliersForm from '../components/Forms/AddSuppliersForm';
+import CustomersInfoTable from '../components/Tables/CustomersInfoTable';
+import EditProductsForm from '../components/Forms/EditProductsForm';
+import EditSuppliersForm from '../components/Forms/EditSuppliersForm';
+import EditCustomersForm from '../components/Forms/EditCustomersForm';
+
+import Sidebar from '../components/Sidebar/Sidebar';
+
+const AppRoutes = () => {
+  const [products, setProducts] = useState([]);
+  const [suppliers, setSuppliers] = useState([]);
+  const [customers, setCustomers] = useState([]);
+
+  const addProduct = (product) => {
+    setProducts((prevProducts) => [...prevProducts, product]);
+  };
+
+  const addSupplier = (supplier) => {
+    setSuppliers((prevSuppliers) => [...prevSuppliers, supplier]);
+  };
+
+  const addCustomer = (customer) => {
+    setCustomers((prevCustomers) => [...prevCustomers, customer]);
+  };
+
+  const updateProduct = (updatedProduct) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.productId === updatedProduct.productId
+          ? updatedProduct
+          : product,
+      ),
+    );
+  };
+
+  const updateSupplier = (updatedSupplier) => {
+    setSuppliers((prevSuppliers) =>
+      prevSuppliers.map((supplier) =>
+        supplier.supplierId === updatedSupplier.supplierId
+          ? updatedSupplier
+          : supplier,
+      ),
+    );
+  };
+
+  const updateCustomer = (updatedCustomer) => {
+    setCustomers((prevCustomers) =>
+      prevCustomers.map((customer) =>
+        customer.customerId === updatedCustomer.customerId
+          ? updatedCustomer
+          : customer,
+      ),
+    );
+  };
+
+  return (
+    <Routes>
+      <Route
+        path='/add-products'
+        element={<AddProductsForm onAddProduct={addProduct} />}
+      />
+      <Route
+        path='/products-info'
+        element={<ProductsInfoTable products={products} />}
+      />
+      <Route
+        path='/edit-product/:productId'
+        element={<EditProductsForm onUpdateProduct={updateProduct} />}
+      />
+
+      <Route
+        path='/add-suppliers'
+        element={<AddSuppliersForm onAddSupplier={addSupplier} />}
+      />
+      <Route
+        path='/suppliers-info'
+        element={<SuppliersInfoTable suppliers={suppliers} />}
+      />
+      <Route
+        path='/edit-supplier/:supplierId'
+        element={<EditSuppliersForm onUpdateSupplier={updateSupplier} />}
+      />
+
+      <Route
+        path='/add-customers'
+        element={<AddCustomersForm onAddCustomer={addCustomer} />}
+      />
+      <Route
+        path='/customers-info'
+        element={<CustomersInfoTable customers={customers} />}
+      />
+      <Route
+        path='/edit-customer/:customerId'
+        element={<EditCustomersForm onUpdateCustomer={updateCustomer} />}
+      />
+
+      <Route path='/sidebar' element={<Sidebar />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
