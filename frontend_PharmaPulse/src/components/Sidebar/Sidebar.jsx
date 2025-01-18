d9ef51b74577ce4bdad3c9cacf49a0e350c6ba53
Azha <157900694+AzhaFauz@users.noreply.github.com>
2025-01-18 13:21:50 +0530

    Created common sidebar for all dashboards

--- frontend_PharmaPulse/src/components/Sidebar/Sidebar.jsx
+++ frontend_PharmaPulse/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
+import sidebarSections from "../../sidebarSections.json";
 
 const SidebarLink = ({ to, children }) => (
   <li className="mb-[10px]">
@@ -23,40 +24,49 @@ SidebarLink.propTypes = {
   children: PropTypes.node.isRequired, // Ensuring 'children' is required and can be any renderable content
 };
 
-const Sidebar = () => {
+const SidebarSection = ({ title, links }) => (
+  <div className="mb-5">
+    <h2 className="text-[20px] mb-2 mt-4 border-b border-[#2d6a6c] pb-1 font-bold">
+      {title}
+    </h2>
+    <ul className="list-none p-0">
+      {links.map((link, index) => (
+        <SidebarLink key={index} to={link.to}>
+          {link.name}
+        </SidebarLink>
+      ))}
+    </ul>
+  </div>
+);
+
+SidebarSection.propTypes = {
+  title: PropTypes.string.isRequired,
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      to: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+const Sidebar = ({ role }) => {
+  const sections = sidebarSections[role] || [];
+
   return (
     <aside className="fixed top-[100px] bottom-[70px] left-0 w-[250px] bg-[#c1d4e0] text-[#494949] overflow-y-auto shadow-md p-5 z-[999] box-border">
-      <div className="mb-5">
-        <h2 className="text-[20px] mb-2 mt-4 border-b border-[#2d6a6c] pb-1 font-bold">
-          Products
-        </h2>
-        <ul className="list-none p-0">
-          <SidebarLink to="/add-products">Add Products</SidebarLink>
-          <SidebarLink to="/products-info">Products Info</SidebarLink>
-        </ul>
-      </div>
-
-      <div className="mb-5">
-        <h2 className="text-[20px] mb-2 mt-4 border-b border-[#2d6a6c] pb-1 font-bold">
-          Suppliers
-        </h2>
-        <ul className="list-none p-0">
-          <SidebarLink to="/add-suppliers">Add Suppliers</SidebarLink>
-          <SidebarLink to="/suppliers-info">Suppliers Info</SidebarLink>
-        </ul>
-      </div>
-
-      <div>
-        <h2 className="text-[20px] mb-2 mt-4 border-b border-[#2d6a6c] pb-1 font-bold">
-          Customers
-        </h2>
-        <ul className="list-none p-0">
-          <SidebarLink to="/add-customers">Add Customers</SidebarLink>
-          <SidebarLink to="/customers-info">Customers Info</SidebarLink>
-        </ul>
-      </div>
+      {sections.map((section, index) => (
+        <SidebarSection
+          key={index}
+          title={section.title}
+          links={section.links}
+        />
+      ))}
     </aside>
   );
+};
+
+Sidebar.propTypes = {
+  role: PropTypes.string.isRequired, // Role will determine which sections to show
 };
 
 export default Sidebar;
