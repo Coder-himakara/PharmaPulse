927dc3d1b673bb826e25f2ea03c132c79d4ab551
Azha <157900694+AzhaFauz@users.noreply.github.com>
2025-02-02 23:17:07 +0530

    Updated codes

--- frontend_PharmaPulse/src/components/Sidebar/Sidebar.jsx
+++ frontend_PharmaPulse/src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,17 @@
-import { NavLink } from "react-router-dom";
-import PropTypes from "prop-types";
-import sidebarSections from "../../sidebarSections.json";
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import sidebarSections from '../../sidebarSections.json';
 
 // SidebarLink Component
 const SidebarLink = ({ to, children }) => (
-  <li className="mb-2">
+  <li className='mb-2'>
     <NavLink
       to={to}
       className={({ isActive }) =>
         `block text-base p-2 rounded-md transition-all duration-300 ${
           isActive
-            ? "bg-teal-700 text-white"
-            : "text-gray-700 hover:bg-teal-700 hover:text-teal-200 dark:text-gray-300 dark:hover:text-teal-400"
+            ? 'bg-teal-700 text-white'
+            : 'text-gray-700 hover:bg-teal-700 hover:text-teal-200 dark:text-gray-300 dark:hover:text-teal-400'
         }`
       }
     >
@@ -27,11 +27,11 @@ SidebarLink.propTypes = {
 
 // SidebarSection Component
 const SidebarSection = ({ title, links }) => (
-  <div className="mb-6">
-    <h2 className="pb-1 mb-3 text-lg font-semibold border-b border-teal-700 dark:border-teal-400">
+  <div className='mb-6'>
+    <h2 className='pb-1 mb-3 text-lg font-semibold border-b border-teal-700 dark:border-teal-400'>
       {title}
     </h2>
-    <ul className="p-0 list-none">
+    <ul className='p-0 list-none'>
       {links.map((link, index) => (
         <SidebarLink key={index} to={link.to}>
           {link.name}
@@ -47,7 +47,7 @@ SidebarSection.propTypes = {
     PropTypes.shape({
       to: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
-    })
+    }),
   ).isRequired,
 };
 
@@ -56,7 +56,7 @@ const Sidebar = ({ role }) => {
   const sections = sidebarSections[role] || [];
 
   return (
-    <aside className="fixed top-[100px] bottom-[70px] left-0 w-[250px] bg-[#C1D4E0] dark:bg-gray-800 text-gray-800 dark:text-gray-200 overflow-y-auto shadow-lg p-5 z-[999] box-border">
+    <aside className='fixed top-[100px] bottom-[70px] left-0 w-[250px] bg-[#C1D4E0] dark:bg-gray-800 text-gray-800 dark:text-gray-200 overflow-y-auto shadow-lg p-5 z-[999] box-border'>
       {sections.map((section, index) => (
         <SidebarSection
           key={index}
