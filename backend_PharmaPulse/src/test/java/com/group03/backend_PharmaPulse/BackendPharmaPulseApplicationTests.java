3aff7c13763146ca0703e936bae6885f39e5b5ed
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2025-02-08 13:43:32 +0530

    updated files

--- backend_PharmaPulse/src/test/java/com/group03/backend_PharmaPulse/BackendPharmaPulseApplicationTests.java
+++ backend_PharmaPulse/src/test/java/com/group03/backend_PharmaPulse/BackendPharmaPulseApplicationTests.java
@@ -2,9 +2,19 @@ package com.group03.backend_PharmaPulse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BackendPharmaPulseApplicationTests {
+
+	@Container
+	public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine")
+			.withDatabaseName("test_db")
+			.withUsername("test_user")
+			.withPassword("test_password");
 
 	@Test
 	void contextLoads() {
