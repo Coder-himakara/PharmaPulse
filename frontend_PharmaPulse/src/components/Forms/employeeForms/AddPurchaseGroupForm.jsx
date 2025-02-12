242b4c4c224b97266c94d7dd69c85f2ae9f40fee
AzhaFauz <adhuha12432@usci.ruh.ac.lk>
2025-02-12 18:54:32 +0530

    Updated to the correct format

--- frontend_PharmaPulse/src/components/Forms/employeeForms/AddPurchaseGroupForm.jsx
+++ frontend_PharmaPulse/src/components/Forms/employeeForms/AddPurchaseGroupForm.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 const AddPurchaseGroupForm = ({ onAddPurchaseGroup }) => {
   const [formData, setFormData] = useState({
-    purchaseGroupName: "",
-    address: "",
-    contactName: "",
-    telePhoneNo: "",
-    email: "",
-    fax: "",
+    purchaseGroupName: '',
+    address: '',
+    contactName: '',
+    telePhoneNo: '',
+    email: '',
+    fax: '',
   });
 
   const navigate = useNavigate();
 
-  const [successMessage, setSuccessMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -36,12 +36,12 @@ const AddPurchaseGroupForm = ({ onAddPurchaseGroup }) => {
       !formData.email.trim() ||
       !formData.fax.trim()
     ) {
-      setErrorMessage("Please fill out all required fields.");
+      setErrorMessage('Please fill out all required fields.');
       return;
     }
 
-    setErrorMessage("");
-    setSuccessMessage("Purchase Group Added Successfully!");
+    setErrorMessage('');
+    setSuccessMessage('Purchase Group Added Successfully!');
 
     if (onAddPurchaseGroup) {
       onAddPurchaseGroup(formData);
@@ -49,159 +49,159 @@ const AddPurchaseGroupForm = ({ onAddPurchaseGroup }) => {
 
     setTimeout(() => {
       setFormData({
-        purchaseGroupId: "",
-        purchaseGroupName: "",
-        address: "",
-        contactName: "",
-        telePhoneNo: "",
-        email: "",
-        fax: "",
+        purchaseGroupId: '',
+        purchaseGroupName: '',
+        address: '',
+        contactName: '',
+        telePhoneNo: '',
+        email: '',
+        fax: '',
       });
-      setSuccessMessage("");
+      setSuccessMessage('');
     }, 2000);
   };
 
   const handleCancel = () => {
-    navigate("/home");
+    navigate('/home');
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col max-w-md mx-auto p-5 bg-[#e6eef3] rounded-lg shadow-md"
+      className='flex flex-col max-w-md mx-auto p-5 bg-[#e6eef3] rounded-lg shadow-md'
     >
-      <h2 className="text-center bg-[#1a5353] text-white p-2 rounded-t-md -mx-5 mt-[-32px] mb-5 text-lg">
+      <h2 className='text-center bg-[#1a5353] text-white p-2 rounded-t-md -mx-5 mt-[-32px] mb-5 text-lg'>
         Add Purchase Group
       </h2>
 
       {errorMessage && (
-        <p className="text-[#991919] text-sm font-bold mb-4">{errorMessage}</p>
+        <p className='text-[#991919] text-sm font-bold mb-4'>{errorMessage}</p>
       )}
       {successMessage && (
-        <p className="text-[#3c5f3c] text-sm font-bold mb-4">
+        <p className='text-[#3c5f3c] text-sm font-bold mb-4'>
           {successMessage}
         </p>
       )}
 
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="purchaseGroupName"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='purchaseGroupName'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Purchase Group Name:
         </label>
         <input
-          type="text"
-          id="purchaseGroupName"
-          name="purchaseGroupName"
+          type='text'
+          id='purchaseGroupName'
+          name='purchaseGroupName'
           value={formData.purchaseGroupName}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="purchaseGroupId"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='purchaseGroupId'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Purchase Group Id:
         </label>
         <input
-          type="text"
-          id="purchaseGroupId"
-          name="purchaseGroupId"
+          type='text'
+          id='purchaseGroupId'
+          name='purchaseGroupId'
           value={formData.purchaseGroupId}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
-        <label htmlFor="address" className="text-[16px] text-gray-800 w-2/3">
+      <div className='flex items-center justify-between mb-4'>
+        <label htmlFor='address' className='text-[16px] text-gray-800 w-2/3'>
           Address:
         </label>
         <input
-          type="text"
-          id="address"
-          name="address"
+          type='text'
+          id='address'
+          name='address'
           value={formData.address}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="contactName"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='contactName'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Contact Name:
         </label>
         <input
-          type="text"
-          id="contactName"
-          name="contactName"
+          type='text'
+          id='contactName'
+          name='contactName'
           value={formData.contactName}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="telePhoneNo"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='telePhoneNo'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Telephone No:
         </label>
         <input
-          type="number"
-          id="telePhoneNo"
-          name="telePhoneNo"
+          type='number'
+          id='telePhoneNo'
+          name='telePhoneNo'
           value={formData.telePhoneNo}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
-        <label htmlFor="email" className="text-[16px] text-gray-800 w-2/3">
+      <div className='flex items-center justify-between mb-4'>
+        <label htmlFor='email' className='text-[16px] text-gray-800 w-2/3'>
           Email:
         </label>
         <input
-          type="email"
-          id="email"
-          name="email"
+          type='email'
+          id='email'
+          name='email'
           value={formData.email}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
-        <label htmlFor="fax" className="text-[16px] text-gray-800 w-2/3">
+      <div className='flex items-center justify-between mb-4'>
+        <label htmlFor='fax' className='text-[16px] text-gray-800 w-2/3'>
           Fax:
         </label>
         <input
-          id="fax"
-          name="fax"
+          id='fax'
+          name='fax'
           value={formData.fax}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm text-gray-800 border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm text-gray-800 border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex justify-center gap-2 mt-5">
+      <div className='flex justify-center gap-2 mt-5'>
         <button
-          type="submit"
-          className="px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]"
+          type='submit'
+          className='px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]'
         >
           Add
         </button>
         <button
-          type="button"
+          type='button'
           onClick={handleCancel}
-          className="px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]"
+          className='px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]'
         >
           Cancel
         </button>
--- frontend_PharmaPulse/src/components/Forms/employeeForms/EditSuppliersForm.jsx
+++ frontend_PharmaPulse/src/components/Forms/employeeForms/EditSuppliersForm.jsx
@@ -1,25 +1,25 @@
-import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { useNavigate, useLocation } from "react-router-dom";
-import { FaSearch } from "react-icons/fa"; // Importing search icon
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa'; // Importing search icon
 
 const EditSuppliersForm = ({ onUpdateSupplier }) => {
   const { state } = useLocation(); // Access the state passed by navigate
   const supplier = state?.supplier; // Get supplier from the state
 
   const [formData, setFormData] = useState({
-    supplierName: "",
-    supplierAddress: "",
-    contactNumber: "",
-    purchaseGroup: "",
-    creditPeriod: "",
-    creditLimit: "",
+    supplierName: '',
+    supplierAddress: '',
+    contactNumber: '',
+    purchaseGroup: '',
+    creditPeriod: '',
+    creditLimit: '',
   });
 
   const navigate = useNavigate();
 
-  const [successMessage, setSuccessMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     if (supplier) {
@@ -47,159 +47,159 @@ const EditSuppliersForm = ({ onUpdateSupplier }) => {
 
     // Basic validation
     if (!formData.email || !formData.address || !formData.contactNumber) {
-      setErrorMessage("Please fill out all required fields.");
+      setErrorMessage('Please fill out all required fields.');
       return;
     }
 
-    setErrorMessage(""); // Clear errors
+    setErrorMessage(''); // Clear errors
 
     // Pass the updated supplier data to the parent
     if (onUpdateSupplier) {
       onUpdateSupplier(formData);
     }
 
-    setSuccessMessage("Supplier updated successfully!");
+    setSuccessMessage('Supplier updated successfully!');
 
     // Clear the form and success message after a delay
     setTimeout(() => {
-      setSuccessMessage("");
-      navigate("/suppliers-info");
+      setSuccessMessage('');
+      navigate('/suppliers-info');
     }, 2000);
   };
 
   const handleCancel = () => {
-    navigate("/suppliers-info");
+    navigate('/suppliers-info');
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col max-w-md mx-auto p-5 bg-[#e6eef3] rounded-lg shadow-md"
+      className='flex flex-col max-w-md mx-auto p-5 bg-[#e6eef3] rounded-lg shadow-md'
     >
-      <h2 className="text-center bg-[#1a5353] text-white p-2 rounded-t-md -mx-5 mt-[-20px] mb-5 text-lg">
+      <h2 className='text-center bg-[#1a5353] text-white p-2 rounded-t-md -mx-5 mt-[-20px] mb-5 text-lg'>
         Edit Supplier
       </h2>
 
       {errorMessage && (
-        <p className="text-[#991919] text-sm font-bold mb-4">{errorMessage}</p>
+        <p className='text-[#991919] text-sm font-bold mb-4'>{errorMessage}</p>
       )}
       {successMessage && (
-        <p className="text-[#3c5f3c] text-sm font-bold mb-4">
+        <p className='text-[#3c5f3c] text-sm font-bold mb-4'>
           {successMessage}
         </p>
       )}
 
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="supplierName"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='supplierName'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Supplier Name:
         </label>
         <input
-          type="text"
-          id="supplierName"
-          name="supplierName"
+          type='text'
+          id='supplierName'
+          name='supplierName'
           value={formData.supplierName}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="supplierAddress"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='supplierAddress'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Supplier Address:
         </label>
         <input
-          type="text"
-          id="supplierAddress"
-          name="supplierAddress"
+          type='text'
+          id='supplierAddress'
+          name='supplierAddress'
           value={formData.supplierAddress}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="contactNumber"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='contactNumber'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Contact Number:
         </label>
         <input
-          type="text"
-          id="contactNumber"
-          name="contactNumber"
+          type='text'
+          id='contactNumber'
+          name='contactNumber'
           value={formData.contactNumber}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="purchaseGroup"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='purchaseGroup'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Purchase Group:
         </label>
         <input
-          type="text"
-          id="purchaseGroup"
-          name="purchaseGroup"
+          type='text'
+          id='purchaseGroup'
+          name='purchaseGroup'
           value={formData.purchaseGroup}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
-        <FaSearch className="absolute text-gray-500 transform -translate-y-1/2 top-1/2 right-3" />
+        <FaSearch className='absolute text-gray-500 transform -translate-y-1/2 top-1/2 right-3' />
       </div>
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="creditPeriod"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='creditPeriod'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Credit Period:
         </label>
         <input
-          type="text"
-          id="creditPeriod"
-          name="creditPeriod"
+          type='text'
+          id='creditPeriod'
+          name='creditPeriod'
           value={formData.creditPeriod}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="creditLimit"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='creditLimit'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Credit Limit:
         </label>
         <input
-          type="text"
-          id="creditLimit"
-          name="creditLimit"
+          type='text'
+          id='creditLimit'
+          name='creditLimit'
           value={formData.creditLimit}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
-      <div className="flex justify-center gap-2 mt-5">
+      <div className='flex justify-center gap-2 mt-5'>
         <button
-          type="submit"
-          className="px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]"
+          type='submit'
+          className='px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]'
         >
           Update
         </button>
         <button
-          type="button"
+          type='button'
           onClick={handleCancel}
-          className="px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]"
+          className='px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]'
         >
           Cancel
         </button>
