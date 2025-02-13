b1c7c3005779823dce76d75bae1b25f478e65139
AzhaFauz <adhuha12432@usci.ruh.ac.lk>
2025-02-13 12:07:27 +0530

    Updated

--- frontend_PharmaPulse/src/components/Forms/employeeForms/EditCustomersForm.jsx
+++ frontend_PharmaPulse/src/components/Forms/employeeForms/EditCustomersForm.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa'; // Importing search icon
 
 const EditCustomersForm = ({ onUpdateCustomer }) => {
   const { state } = useLocation(); // Access the state passed by navigate
@@ -9,33 +10,33 @@ const EditCustomersForm = ({ onUpdateCustomer }) => {
   // Function to format date (DD-MMM-YYYY)
   const formatDate = (date) => {
     return date
-      .toLocaleDateString("en-GB", {
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
+      .toLocaleDateString('en-GB', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
       })
-      .replace(/ /g, "-"); // Example: 11-Feb-2025
+      .replace(/ /g, '-'); // Example: 11-Feb-2025
   };
 
   const [formData, setFormData] = useState({
-    customerName: "",
-    address: "",
-    contactName: "",
-    nic: "",
-    brcNo: "",
-    email: "",
-    phoneNo: "",
-    customerGroup: "",
-    status: "",
+    customerName: '',
+    address: '',
+    contactName: '',
+    nic: '',
+    brcNo: '',
+    email: '',
+    phoneNo: '',
+    customerGroup: '',
+    status: '',
     registeredDate: formatDate(new Date()), // Default to current date if not provided
-    creditLimit: "",
-    creditPeriod: "",
+    creditLimit: '',
+    creditPeriod: '',
   });
 
   const navigate = useNavigate();
 
-  const [successMessage, setSuccessMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     if (customer) {
@@ -71,262 +72,262 @@ const EditCustomersForm = ({ onUpdateCustomer }) => {
 
     // Basic validation
     if (!formData) {
-      setErrorMessage("Please fill out all required fields.");
+      setErrorMessage('Please fill out all required fields.');
       return;
     }
 
-    setErrorMessage(""); // Clear errors
+    setErrorMessage(''); // Clear errors
 
     // Pass the updated customer data to the parent
     if (onUpdateCustomer) {
       onUpdateCustomer(formData);
     }
 
-    setSuccessMessage("Customer updated successfully!");
+    setSuccessMessage('Customer updated successfully!');
 
     // Redirect after update
     setTimeout(() => {
-      setSuccessMessage("");
-      navigate("/customers-info");
+      setSuccessMessage('');
+      navigate('/customers-info');
     }, 2000);
   };
 
   const handleCancel = () => {
-    navigate("/customers-info");
+    navigate('/customers-info');
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col max-w-md mx-auto p-5 bg-[#e6eef3] rounded-lg shadow-md"
+      className='flex flex-col max-w-md mx-auto p-5 bg-[#e6eef3] rounded-lg shadow-md'
     >
-      <h2 className="text-center bg-[#1a5353] text-white p-2 rounded-t-md -mx-5 mt-[-32px] mb-5 text-lg">
+      <h2 className='text-center bg-[#1a5353] text-white p-2 rounded-t-md -mx-5 mt-[-32px] mb-5 text-lg'>
         Edit Customer
       </h2>
 
       {errorMessage && (
-        <p className="text-[#991919] text-sm font-bold mb-4">{errorMessage}</p>
+        <p className='text-[#991919] text-sm font-bold mb-4'>{errorMessage}</p>
       )}
       {successMessage && (
-        <p className="text-[#3c5f3c] text-sm font-bold mb-4">
+        <p className='text-[#3c5f3c] text-sm font-bold mb-4'>
           {successMessage}
         </p>
       )}
 
       {/* Customer Name */}
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="customerName"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='customerName'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Customer Name:
         </label>
         <input
-          type="text"
-          id="customerName"
-          name="customerName"
+          type='text'
+          id='customerName'
+          name='customerName'
           value={formData.customerName}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
       {/* Address */}
-      <div className="flex items-center justify-between mb-4">
-        <label htmlFor="address" className="text-[16px] text-gray-800 w-2/3">
+      <div className='flex items-center justify-between mb-4'>
+        <label htmlFor='address' className='text-[16px] text-gray-800 w-2/3'>
           Address:
         </label>
         <input
-          type="text"
-          id="address"
-          name="address"
+          type='text'
+          id='address'
+          name='address'
           value={formData.address}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
       {/* Contact Name */}
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="contactName"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='contactName'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Contact Name:
         </label>
         <input
-          type="text"
-          id="contactName"
-          name="contactName"
+          type='text'
+          id='contactName'
+          name='contactName'
           value={formData.contactName}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
       {/* NIC */}
-      <div className="flex items-center justify-between mb-4">
-        <label htmlFor="nic" className="text-[16px] text-gray-800 w-2/3">
+      <div className='flex items-center justify-between mb-4'>
+        <label htmlFor='nic' className='text-[16px] text-gray-800 w-2/3'>
           NIC:
         </label>
         <input
-          type="text"
-          id="nic"
-          name="nic"
+          type='text'
+          id='nic'
+          name='nic'
           value={formData.nic}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
       {/* NIC */}
-      <div className="flex items-center justify-between mb-4">
-        <label htmlFor="brcNo" className="text-[16px] text-gray-800 w-2/3">
+      <div className='flex items-center justify-between mb-4'>
+        <label htmlFor='brcNo' className='text-[16px] text-gray-800 w-2/3'>
           Businesses Registration Number:
         </label>
         <input
-          type="text"
-          id="brcNo"
-          name="brcNo"
+          type='text'
+          id='brcNo'
+          name='brcNo'
           value={formData.brcNo}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
-        <label htmlFor="email" className="text-[16px] text-gray-800 w-2/3">
+      <div className='flex items-center justify-between mb-4'>
+        <label htmlFor='email' className='text-[16px] text-gray-800 w-2/3'>
           Email:
         </label>
         <input
-          type="email"
-          id="email"
-          name="email"
+          type='email'
+          id='email'
+          name='email'
           value={formData.email}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
-        <label htmlFor="phoneNo" className="text-[16px] text-gray-800 w-2/3">
+      <div className='flex items-center justify-between mb-4'>
+        <label htmlFor='phoneNo' className='text-[16px] text-gray-800 w-2/3'>
           Phone Number:
         </label>
         <input
-          type="number"
-          id="phoneNo"
-          name="phoneNo"
+          type='number'
+          id='phoneNo'
+          name='phoneNo'
           value={formData.phoneNo}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-red-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-red-300 rounded-md'
         />
       </div>
 
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="customerGroup"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='customerGroup'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Customer Group:
         </label>
-        <div className="relative w-2/3">
+        <div className='relative w-2/3'>
           <input
-            type="text"
-            id="customerGroup"
-            name="customerGroup"
+            type='text'
+            id='customerGroup'
+            name='customerGroup'
             value={formData.customerGroup}
             onChange={handleChange}
-            className="w-full px-2 py-2 text-sm border border-gray-300 rounded-md"
+            className='w-full px-2 py-2 text-sm border border-gray-300 rounded-md'
           />
-          <FaSearch className="absolute text-gray-500 transform -translate-y-1/2 top-1/2 right-3" />
+          <FaSearch className='absolute text-gray-500 transform -translate-y-1/2 top-1/2 right-3' />
         </div>
       </div>
 
       {/* Status */}
-      <div className="flex items-center justify-between mb-4">
-        <label htmlFor="status" className="text-[16px] text-gray-800 w-2/3">
+      <div className='flex items-center justify-between mb-4'>
+        <label htmlFor='status' className='text-[16px] text-gray-800 w-2/3'>
           Status:
         </label>
         <select
-          id="status"
-          name="status"
+          id='status'
+          name='status'
           value={formData.status}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         >
-          <option value="">Choose a status</option>
-          <option value="active">ACTIVE</option>
-          <option value="inactive">INACTIVE</option>
-          <option value="suspended">SUSPENDED</option>
+          <option value=''>Choose a status</option>
+          <option value='active'>ACTIVE</option>
+          <option value='inactive'>INACTIVE</option>
+          <option value='suspended'>SUSPENDED</option>
         </select>
       </div>
 
       {/* Registered Date (Read-Only) */}
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="registeredDate"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='registeredDate'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Registered Date:
         </label>
         <input
-          type="text"
-          id="registeredDate"
-          name="registeredDate"
+          type='text'
+          id='registeredDate'
+          name='registeredDate'
           value={formData.registeredDate}
           readOnly
-          className="w-2/3 px-2 py-2 text-sm bg-gray-100 border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm bg-gray-100 border border-gray-300 rounded-md'
         />
       </div>
 
       {/* Credit Limit */}
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="creditLimit"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='creditLimit'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Credit Limit:
         </label>
         <input
-          type="text"
-          id="creditLimit"
-          name="creditLimit"
+          type='text'
+          id='creditLimit'
+          name='creditLimit'
           value={formData.creditLimit}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
       {/* Credit Period */}
-      <div className="flex items-center justify-between mb-4">
+      <div className='flex items-center justify-between mb-4'>
         <label
-          htmlFor="creditPeriod"
-          className="text-[16px] text-gray-800 w-2/3"
+          htmlFor='creditPeriod'
+          className='text-[16px] text-gray-800 w-2/3'
         >
           Credit Period:
         </label>
         <input
-          type="text"
-          id="creditPeriod"
-          name="creditPeriod"
+          type='text'
+          id='creditPeriod'
+          name='creditPeriod'
           value={formData.creditPeriod}
           onChange={handleChange}
-          className="w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md"
+          className='w-2/3 px-2 py-2 text-sm border border-gray-300 rounded-md'
         />
       </div>
 
       {/* Buttons */}
-      <div className="flex justify-center gap-2 mt-5">
+      <div className='flex justify-center gap-2 mt-5'>
         <button
-          type="submit"
-          className="px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]"
+          type='submit'
+          className='px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]'
         >
           Update
         </button>
         <button
-          type="button"
+          type='button'
           onClick={handleCancel}
-          className="px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]"
+          className='px-5 py-2 bg-[#2a4d69] text-white border-none rounded-md text-[16px] cursor-pointer transition-all duration-300 hover:bg-[#00796b]'
         >
           Cancel
         </button>
--- frontend_PharmaPulse/src/components/Tables/employeeTables/CustomersInfoTable.jsx
+++ frontend_PharmaPulse/src/components/Tables/employeeTables/CustomersInfoTable.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 const CustomersInfoTable = ({ customers }) => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
   const navigate = useNavigate();
 
   const filteredCustomers = customers.filter((customer) =>
-    customer.customerName.toLowerCase().includes(search.toLowerCase())
+    customer.customerName.toLowerCase().includes(search.toLowerCase()),
   );
 
   const handleClose = () => {
-    navigate("/home");
+    navigate('/home');
   };
 
   const handleEdit = (customerId) => {
@@ -24,105 +24,105 @@ const CustomersInfoTable = ({ customers }) => {
   };
 
   return (
-    <div className="bg-[#e6eef3] rounded-lg shadow-lg mb-5 pb-5 h-full relative">
-      <div className="bg-[#1a5353] text-white px-4 py-3 text-left rounded-t-lg m-0 relative">
-        <h1 className="p-1 m-1 text-2xl">Customers Management</h1>
+    <div className='bg-[#e6eef3] rounded-lg shadow-lg mb-5 pb-5 h-full relative'>
+      <div className='bg-[#1a5353] text-white px-4 py-3 text-left rounded-t-lg m-0 relative'>
+        <h1 className='p-1 m-1 text-2xl'>Customers Management</h1>
         <button
-          className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-none text-white border-none text-2xl cursor-pointer hover:text-[#f1f1f1] mr-4"
+          className='absolute top-1/2 right-2 transform -translate-y-1/2 bg-none text-white border-none text-2xl cursor-pointer hover:text-[#f1f1f1] mr-4'
           onClick={handleClose}
         >
           X
         </button>
       </div>
 
-      <div className="flex items-center justify-between p-2 m-2">
-        <h2 className="text-2xl font-bold text-[#1a5353]">Customers</h2>
-        <div className="relative">
+      <div className='flex items-center justify-between p-2 m-2'>
+        <h2 className='text-2xl font-bold text-[#1a5353]'>Customers</h2>
+        <div className='relative'>
           <input
-            type="text"
-            placeholder="Search Customers..."
+            type='text'
+            placeholder='Search Customers...'
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            className="px-3 py-2 border border-[#ccc] rounded-md text-sm w-[400px]"
+            className='px-3 py-2 border border-[#ccc] rounded-md text-sm w-[400px]'
           />
         </div>
       </div>
 
       {filteredCustomers.length === 0 && search && (
-        <div className="text-[#991919] text-sm text-center mt-2 font-bold">
+        <div className='text-[#991919] text-sm text-center mt-2 font-bold'>
           No customers found matching your search.
         </div>
       )}
 
-      <div className="p-2 m-2">
-        <table className="w-full border-collapse">
+      <div className='p-2 m-2'>
+        <table className='w-full border-collapse'>
           <thead>
             <tr>
-              <th className="border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm">
+              <th className='border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm'>
                 Customer ID
               </th>
-              <th className="border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm">
+              <th className='border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm'>
                 Customer Name
               </th>
-              <th className="border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm">
+              <th className='border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm'>
                 Status
               </th>
-              <th className="border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm">
+              <th className='border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm'>
                 Credit Limit
               </th>
-              <th className="border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm">
+              <th className='border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm'>
                 Credit Period
               </th>
-              <th className="border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm">
+              <th className='border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm'>
                 Email
               </th>
-              <th className="border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm">
+              <th className='border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm'>
                 Phone Number
               </th>
-              <th className="border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm">
+              <th className='border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm'>
                 Contact Name
               </th>
-              <th className="border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm">
+              <th className='border border-[#bfb6b6] p-2 text-center bg-[#ffb24d] text-[#5e5757] text-sm'>
                 Action
               </th>
             </tr>
           </thead>
           <tbody>
             {filteredCustomers.map((customer, index) => (
-              <tr key={index} className="bg-[#c6dceb] hover:bg-[#dce4e9]">
-                <td className="border border-[#bfb6b6] p-2 text-center text-sm">
+              <tr key={index} className='bg-[#c6dceb] hover:bg-[#dce4e9]'>
+                <td className='border border-[#bfb6b6] p-2 text-center text-sm'>
                   {customer.customerId}
                 </td>
-                <td className="border border-[#bfb6b6] p-2 text-center text-sm">
+                <td className='border border-[#bfb6b6] p-2 text-center text-sm'>
                   {customer.customerName}
                 </td>
-                <td className="border border-[#bfb6b6] p-2 text-center text-sm">
+                <td className='border border-[#bfb6b6] p-2 text-center text-sm'>
                   {customer.status}
                 </td>
-                <td className="border border-[#bfb6b6] p-2 text-center text-sm">
+                <td className='border border-[#bfb6b6] p-2 text-center text-sm'>
                   {customer.creditLimit}
                 </td>
-                <td className="border border-[#bfb6b6] p-2 text-center text-sm">
+                <td className='border border-[#bfb6b6] p-2 text-center text-sm'>
                   {customer.creditPeriod}
                 </td>
-                <td className="border border-[#bfb6b6] p-2 text-center text-sm">
+                <td className='border border-[#bfb6b6] p-2 text-center text-sm'>
                   {customer.email}
                 </td>
-                <td className="border border-[#bfb6b6] p-2 text-center text-sm">
+                <td className='border border-[#bfb6b6] p-2 text-center text-sm'>
                   {customer.phoneNo}
                 </td>
-                <td className="border border-[#bfb6b6] p-2 text-center text-sm">
+                <td className='border border-[#bfb6b6] p-2 text-center text-sm'>
                   {customer.contactName}
                 </td>
-                <td className="border border-[#bfb6b6] p-2 text-center text-sm">
+                <td className='border border-[#bfb6b6] p-2 text-center text-sm'>
                   <button
-                    className="bg-[#4c85a6] text-white py-1 px-3 rounded-md cursor-pointer text-sm hover:bg-[#15375c] mr-2"
+                    className='bg-[#4c85a6] text-white py-1 px-3 rounded-md cursor-pointer text-sm hover:bg-[#15375c] mr-2'
                     onClick={() => handleEdit(customer.customerId)}
                   >
                     Edit
                   </button>
                   <button
-                    className="bg-[#4c85a6] text-white py-1 px-3 rounded-md cursor-pointer text-sm hover:bg-[#15375c] mr-2"
+                    className='bg-[#4c85a6] text-white py-1 px-3 rounded-md cursor-pointer text-sm hover:bg-[#15375c] mr-2'
                     onClick={() => handleViewCustomer(customer)}
                   >
                     View
@@ -147,7 +147,7 @@ CustomersInfoTable.propTypes = {
       email: PropTypes.string.isRequired,
       phoneNo: PropTypes.string.isRequired,
       contactName: PropTypes.string.isRequired,
-    })
+    }),
   ).isRequired,
 };
 
