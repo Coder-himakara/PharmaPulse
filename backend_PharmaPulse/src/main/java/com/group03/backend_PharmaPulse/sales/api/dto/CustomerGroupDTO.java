6af1c39983051a703473fccedc08d1c386a474be
chalanichanchala22 <chalanichanchala2000@gmail.com>
2025-02-20 09:10:12 +0530

    Add Some Comments for coutomer controller

--- backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/sales/api/dto/CustomerGroupDTO.java
+++ backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/sales/api/dto/CustomerGroupDTO.java
@@ -1,17 +1,8 @@
 package com.group03.backend_PharmaPulse.sales.api.dto;
-
-
-import com.group03.backend_PharmaPulse.sales.internal.entity.Customer;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -19,13 +10,9 @@ import java.util.Set;
 @Builder
 public class CustomerGroupDTO {
 
-    //private Long customerGroupId;
-
     private String customerGroupName;
 
     private String assignedSalesRep;
 
     private String descriptions;
-
-    //private Set<Customer> customers;
 }
--- backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/sales/internal/controller/CustomerController.java
+++ backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/sales/internal/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.group03.backend_PharmaPulse.sales.internal.controller;
 
-import com.group03.backend_PharmaPulse.purchase.api.dto.SupplierDTO;
 import com.group03.backend_PharmaPulse.sales.api.dto.CustomerDTO;
 import com.group03.backend_PharmaPulse.sales.api.event.CustomerService;
 import com.group03.backend_PharmaPulse.util.api.dto.StandardResponse;
@@ -11,15 +10,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("api/customers")
+@RestController //allowing it to handle HTTP requests and return JSON responses.
+@RequestMapping("api/customers") //Sets the base path for all endpoints in this controller (/api/customers).
 public class CustomerController {
     private final CustomerService customerService;
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
 
     }
-    @GetMapping("/all")
+    @GetMapping("/all") //retrieving
     public ResponseEntity<StandardResponse> getAllCustomers() {
         List<CustomerDTO> customerDTOS  = customerService.getAllCustomers();
         return new ResponseEntity<>(
@@ -28,7 +27,7 @@ public class CustomerController {
         );
     }
     @GetMapping("/{id}")
-    public ResponseEntity<StandardResponse> getSuppliersById(@PathVariable Long id) {
+    public ResponseEntity<StandardResponse> getCustomerById(@PathVariable Long id) {
         CustomerDTO selectedCustomer = customerService.getCustomerById(id);
         return new ResponseEntity<>(
                 new StandardResponse(201,"Success",selectedCustomer),
@@ -36,7 +35,7 @@ public class CustomerController {
         );
     }
     // This method is used to add a customer group
-    @PostMapping("/add")
+    @PostMapping("/add") //adding
     public ResponseEntity<StandardResponse> addCustomer(@Valid
                                                          @RequestBody CustomerDTO customerDTO) {
         CustomerDTO savedCustomer=customerService.addCustomer(customerDTO);
@@ -45,7 +44,7 @@ public class CustomerController {
                 HttpStatus.CREATED
         );
     }
-    @PutMapping("/update/{id}")
+    @PutMapping("/update/{id}") //updating
     public ResponseEntity<StandardResponse> updateCustomers(@Valid @PathVariable Long id,
                                                             @RequestBody CustomerDTO customerDTO) {
         CustomerDTO updatedCustomer=customerService.updateCustomer(id,customerDTO);
