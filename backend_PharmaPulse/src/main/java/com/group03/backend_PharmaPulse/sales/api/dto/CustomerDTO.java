e96e1ba67e0776869b48699be5d2bba1ee9175ed
chalanichanchala22 <chalanichanchala2000@gmail.com>
2025-02-20 08:59:19 +0530

    Add Some Commit for Customer DTO

--- backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/sales/api/dto/CustomerDTO.java
+++ backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/sales/api/dto/CustomerDTO.java
@@ -1,17 +1,18 @@
 package com.group03.backend_PharmaPulse.sales.api.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;//Ensures that certain fields (name, address, contact name) cannot be empty.
+import jakarta.validation.constraints.NotNull;//Ensures that the customer_group field must be provided.
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Data // Automatically generates getter, setter, toString(), equals(), and hashCode() methods
+@AllArgsConstructor //Creates a constructor that includes all class variables
+@NoArgsConstructor //Creates a constructor with no parameters.
+@Builder //Helps build objects using a simple and readable way.
+//used to transfer customer-related data in a Spring Boot application
 public class CustomerDTO {
 
     @NotBlank(message = "Customer Name is required")
@@ -25,7 +26,7 @@ public class CustomerDTO {
 
     private String customer_nic_no;
 
-    private String customer_brc_no;
+    private String customer_brc_no; //Business Registration Certificate (BRC) number
 
     private String customer_email;
 
@@ -34,12 +35,12 @@ public class CustomerDTO {
     @NotNull(message = "Please select a customer group")
     private String customer_group;
 
-    private LocalDate registered_date;
+    private LocalDate registered_date;//Stores the date the customer was registered.
 
-    private Double credit_limit;
+    private Double credit_limit;//Represents the maximum credit amount a customer can have
 
-    private Integer credit_period_in_days;
+    private Integer credit_period_in_days;//Defines the period (in days) the customer has to pay the credit.
 
-    private Double outstanding_balance;
+    private Double outstanding_balance;//Stores the amount the customer still needs to pay
 
 }
