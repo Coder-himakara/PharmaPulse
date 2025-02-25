2bd54e44411e38f1254bdca24f7d1c1af8203ec4
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2025-02-25 16:36:11 +0530

    JWT dependencies added

--- backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/config/SecurityConfig.java
+++ backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.group03.backend_PharmaPulse.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,17 +29,38 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(customizer -> customizer.disable());
-        return http.build();
+        return http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request ->request
+                        .requestMatchers("register").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
-
+    /**
+     * This method is used to create an instance of DaoAuthenticationProvider.
+     * Use Database to authenticate the user
+     * @return DaoAuthenticationProvider
+     */
+    @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(userDetailsService);
         return provider;
     }
+    /**
+     * This method is used to create an instance of AuthenticationManager.
+     * @return AuthenticationManager
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
 
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
