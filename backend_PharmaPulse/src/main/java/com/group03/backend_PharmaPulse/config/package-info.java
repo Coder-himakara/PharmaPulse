4da9e9abb9ff7de82ffdc8d80c5216470888207d
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2025-02-27 11:18:38 +0530

    fix(modularity): update package.info

--- backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/config/package-info.java
+++ backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/config/package-info.java
@@ -1,6 +1,6 @@
 @org.springframework.modulith.ApplicationModule(
         type = ApplicationModule.Type.OPEN,
-        allowedDependencies = {"shared", "advisor", "util"}
+        allowedDependencies = {"shared", "advisor", "util","user"}
 )
 package com.group03.backend_PharmaPulse.config;
 
