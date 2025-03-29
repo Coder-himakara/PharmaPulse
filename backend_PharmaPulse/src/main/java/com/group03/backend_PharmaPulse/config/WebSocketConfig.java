dcf85d81a4da196e3423b7b2ed9e442d4ebd4237
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2025-03-29 22:34:46 +0530

    websocket config file created

---
+++ backend_PharmaPulse/src/main/java/com/group03/backend_PharmaPulse/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.group03.backend_PharmaPulse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        //prefix for the messaging channel
+        config.enableSimpleBroker("/topic");
+        //Endpoint for the client to send messages to the server
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+       registry.addEndpoint("/ws")
+               .setAllowedOrigins(" http://localhost:3123/")
+               .withSockJS(); //fallback for browsers that do not support websocket
+    }
+}
