8c6f221daebbb4eb6ed63418943746944a134f21
chalanichanchala22 <chalanichanchala2000@gmail.com>
2025-04-08 17:57:19 +0530

    Employee

--- frontend_PharmaPulse/src/components/Navbar/EmployeeNavbar.jsx
+++ frontend_PharmaPulse/src/components/Navbar/EmployeeNavbar.jsx
@@ -116,8 +116,8 @@ const EmployeeNavbar = () => {
         ],
         [
           {
-            label: "Product Retail Price",
-            items: [{ label: "Retail Price Info",command: () => navigate("/employee-dashboard/retail-price-info") }],
+            label: "Product Price History",
+            items: [{ label: "Whole Sale Price",command: () => navigate("/employee-dashboard/retail-price-info") }],
           },
         ],
       ],
